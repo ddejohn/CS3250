fc8f79a3212fe5fc0cc6ea2e5014c8965ccbe0bb
Devon DeJohn <ddejohn@msudenver.edu>
2019-07-05 15:38:56 -0600

    docstring edits

--- maze_generator.py
+++ maze_generator.py
@@ -1,32 +1,27 @@
 import matplotlib.pyplot as plt
-import numpy
 from numpy import zeros as npz
 from random import shuffle
 from random import choice
 from functools import reduce
 
 
-def main(size=10, draw=False, show_rooms=False, save=False):
-    """Generate a square maze.
+def main(size=10, draw=False, rooms=False, save=False):
+    """Generate a square maze with 'rooms' on intersections, corners, and dead-ends.
 
     Keyword Arguments:
 
-            size : {int} where size % 2 == 0
-                dimension of maze (default: {10})
-            draw : {bool}
-                draw maze as pyplot figure (default: {False})
-            show_rooms : {bool}
-                show/hide rooms on figure (default: {False})
-            save : {bool}
-                save figure to CWD as {size}x{size}.png (default: {False})
+            size {int, default: 10} (where size % 2 == 0): unit width of maze
+            draw {bool, default: False}: show maze as pyplot figure
+            rooms {bool, default: False}: show rooms on figure
+            save {bool, default: False}: save figure to working directory as '{size}x{size}.png'
 
     Returns:
 
-            maze : {list of list of int}
-                a numpy integer matrix
-            rooms : {list of tuple}
-                a list of the room coordinates as {tuple}
+            maze {list of list of int}: a numpy integer matrix
+            rooms {list of tuple}: a {list} of room coordinates as {tuple}
     """
+
+
     moves = [
         [(0, 2), (0, 1)], [(0, -2), (0, -1)],
         [(-2, 0), (-1, 0)], [(2, 0), (1, 0)]
@@ -40,22 +35,22 @@ def main(size=10, draw=False, show_rooms=False, save=False):
 
     if size % 2 != 0:
         print("Maze size must be an even integer!")
-        maze, rooms = None, None
+        maze, nodes = None, None
     else:
-        maze, rooms = _generate_maze(size, show_rooms, moves, rules)
+        maze, nodes = _generate_maze(size, rooms, moves, rules)
         if draw:
             _draw_maze(maze, size, save)
-    return maze, rooms
+    return maze, nodes
 
 
-def _generate_maze(size, show_rooms, moves, rules):
+def _generate_maze(size, rooms, moves, rules):
     m = npz((size+1, size+1), dtype=int)
-    nodes = [(a, b) for a in range(1, size+1, 2) for b in range(1, size+1, 2)]
-    visited = [choice(nodes)]
+    grid = [(a, b) for a in range(1, size+1, 2) for b in range(1, size+1, 2)]
+    visited = [choice(grid)]
     k = visited[0]
-    nodes.remove(k)
+    grid.remove(k)
 
-    while len(nodes) > 0:
+    while len(grid) > 0:
         n = len(visited)
         nsew = []
         for i in range(4):
@@ -65,29 +60,29 @@ def _generate_maze(size, show_rooms, moves, rules):
         shuffle(nsew)
         for a in nsew:
             probe, link = a
-            if probe in nodes:
+            if probe in grid:
                 m[probe], m[link] = 1, 1
-                nodes.remove(probe)
+                grid.remove(probe)
                 visited.extend(a)
                 break
         if n == len(visited):
             k = visited[max(visited.index(k)-1, 1)]
         else:
             k = visited[-1]
-    return _get_rooms(m, visited, show_rooms, rules)
+    return _get_rooms(m, visited, rooms, rules)
 
 
-def _get_rooms(m, visited, show_rooms, rules):
-    rooms = []
+def _get_rooms(m, visited, rooms, rules):
+    nodes = []
     m[visited[0]], m[visited[-2]] = 2, 2
     for coord in visited:
         i, j = coord
         neighbors = [m[i-1, j], m[i+1, j], m[i, j-1], m[i, j+1]]
         if neighbors in rules:
-            rooms.append(coord)
-            if show_rooms:
+            nodes.append(coord)
+            if rooms:
                 m[coord] = 2
-    return m, rooms
+    return m, nodes
 
 
 def _draw_maze(m, size, save):
