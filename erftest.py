560ce91712d8b95658f018f5460f014d3b706185
Devon DeJohn <ddejohn@msudenver.edu>
2019-07-30 11:59:12 -0600

    making python do my MTH-3210 hw

--- erftest.py
+++ erftest.py
@@ -2,12 +2,16 @@ from scipy.special import erfinv
 
 SQ2 = 2.0**0.5
 
+
 def get_z(alpha):
     y = 2*(1-alpha) - 1
     return SQ2*erfinv(y)
+
 
 def get_conf(mean, sigma, n, conf):
     z = 1 - conf/100
     error = get_z(z/2)
     margin = error*sigma/(n**0.5)
     return (mean-margin, mean+margin)
+
+
