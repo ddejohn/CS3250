bd25c83c1ee4199a34c77d4d1fc481ab189cb927
Devon DeJohn <ddejohn@msudenver.edu>
2019-08-03 22:17:15 -0600

    inlays added to legendary and mythical items

--- ItemFactory/factory_data.py
+++ ItemFactory/factory_data.py
@@ -1,3 +1,5 @@
+from random import choice
+
 RARITY = {
     "crude": [100, 70, 0, 0, 0, 0, 0],
     "common": [50, 100, 50, 10, 0, 0, 0],
@@ -28,48 +30,221 @@ ARMOR_MATERIAL = [
 ]
 
 ONE_HANDED = {
-    "dagger": ["fuller", "grip", "pommel", "quillon"],
-    "shortsword": ["fuller", "grip", "pommel", "quillon"],
+    "dagger": ["fuller", "grip", "pommel", choice(["cross-guard", "quillon"])],
+    "shortsword": [
+        "fuller", "grip", "pommel", choice(["cross-guard", "quillon"])
+    ],
     "battle axe": ["pommel", "haft", "hook", "beard"],
     "labrys": ["pommel", "haft", "hook", "beard"]
 }
 
 TWO_HANDED = {
-    "longsword": ["fuller", "grip", "pommel", "quillon"],
-    "recurve bow": ["nock", "limbs", "belly", "grip"],
-    "scythian bow": ["nock", "limbs", "belly", "grip"],
-    "longbow": ["nock", "limbs", "belly", "grip"],
-    "war hammer": ["face", "cheek", "throat", "handle"],
+    "longsword": [
+        "fuller", "grip", "pommel", choice(["cross-guard", "quillon"])
+    ],
+    "recurve bow": ["nock", "face", "belly", "grip"],
+    "scythian bow": ["nock", "face", "belly", "grip"],
+    "longbow": ["nock", "face", "belly", "grip"],
+    "war hammer": ["face", "cheek", "throat", choice(["haft", "handle"])],
 }
 
-DETAIL = {
-    "crude": [],
-    "common": [],
-    "uncommon": [],
-    "rare": [],
-    "legendary": [],
-    "mythical": []
+INLAYS = [
+    "bone",
+    "gold",
+    "silver",
+    "onyx",
+    "obsidian",
+    "amethyst",
+    "emerald",
+    "ebony",
+    "walnut",
+    "ruby",
+    "jade",
+    "turquoise",
+    "ivory",
+    "bronze",
+    "copper",
+    "meteorite",
+    "adamantite",
+    "pearl"
+]
+
+BARBS = [
+    "fangs",
+    "claws",
+    "teeth",
+    "thorns",
+    "spines",
+    "spikes",
+]
+
+DETAIL_ADJECTIVE = {
+    "crude": [
+        "scratched",
+        "pitted",
+        "rusted",
+        "distressed",
+        "chewed",
+        "gnarled",
+        "bent",
+        "scored",
+        "burred"
+    ],
+    "common": [
+        "scratched",
+        "burred",
+        "burnished",
+        "scored",
+        "pitted",
+        "rusted",
+        "distressed"
+    ],
+    "uncommon": [
+        "burnished",
+        "distressed",
+        "scratched",
+        "worn",
+        "dull",
+        "rough"
+    ],
+    "rare": [
+        "polished",
+        "smooth",
+        "gleaming",
+        "elegant",
+        "spotless"
+    ],
+    "legendary": [
+        "ornate",
+        "engraved",
+        "gilded",
+        "elaborate"
+    ],
+    "mythical": [
+        "ornate",
+        "engraved",
+        "gilded",
+        "elaborate"
+    ]
+}
+
+DETAIL_NOUN = {
+    "crude": [
+        "dried blood",
+        "dirt",
+        "scratches",
+        "gashes",
+        "notches",
+        "bits of fur",
+        choice(["teeth marks", "claw marks"])
+    ],
+    "common": [
+        "dried blood",
+        "dirt",
+        "claw marks",
+        "scratches",
+        "filth",
+        "smudges",
+    ],
+    "uncommon": [
+        "smudges",
+        "grit",
+        "scratches",
+        "soot",
+        "residue"
+    ],
+    "rare": [
+        "patina",
+        "etchings",
+        "carvings",
+        "patterns"
+    ],
+    "legendary": [
+        "etchings",
+        "carvings",
+        "patterns",
+        "inscriptions",
+        "engravings",
+    ],
+    "mythical": [
+        "etchings",
+        "carvings",
+        "patterns",
+        "inscriptions",
+        "engravings",
+    ]
+}
+
+DETAIL_VERB = {
+    "crude": [
+        "haphazardly",
+        "shoddily",
+        "crudely",
+        "defectively",
+        "inexpertly",
+        "poorly",
+        "hastily"
+    ],
+    "common": [
+        "haphazardly",
+        "hastily",
+        "inexpertly",
+        "adequately",
+        "competently"
+    ],
+    "uncommon": [
+        "adequately",
+        "competently",
+        "sufficiently",
+        "decently",
+        "capably"
+    ],
+    "rare": [
+        "meticulously",
+        "skillfully",
+        "precisely",
+        "carefully",
+        "fastidiously"
+    ],
+    "legendary": [
+        "expertly",
+        "elegantly",
+        "masterfully",
+        "flawlessly"
+    ],
+    "mythical": [
+        "expertly",
+        "elegantly",
+        "masterfully",
+        "flawlessly"
+    ]
 }
 
 CONDITION = {
     "crude": [
         "ruined",
         "rusty",
+        "marred",
+        "deformed",
+        "tarnished",
+        "blighted",
         "filthy",
-        "burnt",
-        "broken"
+        "decrepit"
     ],
     "common": [
         "worn",
         "chipped",
-        "blemished"
-        "dirty",
+        "blemished",
+        "flawed",
+        "tarnished",
+        "neglected",
         "mended",
     ],
     "uncommon": [
         "etched",
         "carved",
-        "polished",
+        "blemished",
+        "tarnished",
         "mended",
     ],
     "rare": [
@@ -80,8 +255,55 @@ CONDITION = {
     ],
     "legendary": [
         "flawless",
+        "immaculate",
         "pristine",
-        "ornate",
-        "inlaid",
+    ],
+    "mythical": [
+        "flawless",
+        "immaculate",
+        "pristine"
+    ]
+}
+
+QUALITY = {
+    "crude": [
+        "lousy",
+        "mediocre",
+        "dreadful",
+        "inferior",
+        "substandard"
+    ],
+    "common": [
+        "mediocre",
+        "adequate",
+        "common",
+        "middling",
+        "fair"
+    ],
+    "uncommon": [
+        "fair",
+        "suitable",
+        "passable",
+        "acceptable"
+    ],
+    "rare": [
+        "fine",
+        "exceptional",
+        "premium",
+        "refined",
+        "superior"
+    ],
+    "legendary": [
+        "exquisite",
+        "superior",
+        "refined",
+        "impeccable",
+        "superb"
+    ],
+    "mythical": [
+        "exquisite",
+        "superior",
+        "refined",
+        "impeccable",
     ]
 }
--- ItemFactory/item_factory.py
+++ ItemFactory/item_factory.py
@@ -1,4 +1,4 @@
-from random import choice, choices
+from random import choice, choices, randint
 import factory_data
 
 
@@ -61,12 +61,12 @@ class ArmorStats:
         self.luck = int
 
 
-class WeaponItem(WeaponStats, WeaponName, Material):
+class WeaponItem(Material):
     def __init__(self):
         super().__init__()
 
 
-class ArmorItem(ArmorStats, ArmorName, Material):
+class ArmorItem(Material):
     def __init__(self):
         super().__init__()
 
@@ -115,39 +115,91 @@ class ArmorSet:
         self.feet = ArmorFeet
 
 
-class Player(Holder, ArmorSet):
-    pass
+# class Player(Holder, ArmorSet):
+#     pass
 
 
+from random import choice, sample
+def description(item):
+    conditions = shuffled(factory_data.CONDITION[item.rarity])
+    adjectives = shuffled(factory_data.DETAIL_ADJECTIVE[item.rarity])
+    nouns = shuffled(factory_data.DETAIL_NOUN[item.rarity])
+    verbs = shuffled(factory_data.DETAIL_VERB[item.rarity])
+    parts = shuffled(
+        factory_data.ONE_HANDED.get(item.item_type,
+            factory_data.TWO_HANDED.get(item.item_type))
+    )
+    made = shuffled([choice([
+        "formed", "fashioned", "made", "constructed", "assembled"
+    ]), verbs.pop()])
+    quality = factory_data.QUALITY[item.rarity]
+    with_from = choice(["with", "from", "using", "out of"])
+    in_by = choice([
+        "in",
+        "with",
+    ])
+    if item.rarity in ["legendary", "mythical"]:
+        all_parts = ", ".join([parts[0], parts[1], parts[2]])
+        last_sentence = " ".join([
+            f"\nThe {all_parts}, and {parts[3]}",
+            f"are all inlaid with {inlays()}."
+        ])
+    else:
+        last_sentence = " ".join([
+            f"\nThe {is_are(parts[1])} {adjectives.pop()} and",
+            f"{adjectives.pop()}, and the {is_are(parts[2])}",
+            f"covered {in_by} {nouns.pop()} and {nouns.pop()}."
+        ])
+    
+    return " ".join([
+        f"{a_an(conditions.pop()).capitalize()}",
+        f"{set_or_pair(item.item_type)} with",
+        f"{a_an(adjectives.pop())} and {adjectives.pop()} {parts[0]},",
+        f"{made.pop()} {made.pop()} {with_from}",
+        f"{choice(quality)} {item.material}.",
+        last_sentence
+    ])
 
+def shuffled(this):
+    return sample(this, len(this))
 
+def is_are(this):
+    if this[-1] == "s" and this[-2] != "y":
+        return f"{this} are"
+    return f"{this} is"
 
+def set_or_pair(this):
+    if this[-1] == "s" and this[-2] != "y":
+        return choice(["set", "pair"]) + f" of {this}"
+    return this
 
+def a_an(this):
+    if this[0] in ["a", "e", "i", "o", "u"]:
+        return f"an {this}"
+    return f"a {this}"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+def inlays():
+    all_inlays = shuffled(factory_data.INLAYS)
+    these_inlays = []
+    for _ in range(randint(2, 4)):
+        these_inlays.append(all_inlays.pop())
+    last, *rest = these_inlays
+    if rest:
+        return ", ".join(rest) + f", and {last}"
+    return last
 
 if __name__ == "__main__":
     items = [
         WeaponOneHand,
         WeaponTwoHand,
-        ArmorFeet,
-        ArmorChest,
-        ArmorHands,
-        ArmorHead
+        # ArmorFeet,
+        # ArmorChest,
+        # ArmorHands,
+        # ArmorHead
     ]
 
-    for _ in range(100):
-        new_item = choice(items)()
-        print(f"{new_item.rarity} {new_item.material} {new_item.item_type}")
+    for _ in range(50):
+        item = choice(items)()
+        print(f"{item.rarity} {item.material} {item.item_type}:\n")
+        print(description(item))
+        print()
