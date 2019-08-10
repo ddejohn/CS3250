7d534135656da0c9085b9fce558626330241d2cb
Devon DeJohn <ddejohn@msudenver.edu>
2019-08-10 12:43:21 -0600

    pylint

--- item_factory.py
+++ item_factory.py
@@ -187,4 +187,5 @@ if __name__ == "__main__":
         if new_item.item_class.__name__ == "ArmorItem":
             # print(f"{new_item.name}:\n\n{new_item.description}\n")
             print(f"{new_item.name}:\n")
-            print(dictionary_print.verbose_print(vars(new_item.stats), calls=1))
+            print(dictionary_print.verbose_print(
+                vars(new_item.stats), calls=1))
